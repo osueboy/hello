8106905f40fa0474140519494dd130aa2b3d740a
osueboy <osueboy@hotmail.com>
2015-05-01 09:00:51 -0500

    go testing

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/osueboy/newmath"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
